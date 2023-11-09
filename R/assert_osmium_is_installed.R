31cad824a42fe1f95b487e02159ae1e5ed62da4f
Daniel Herszenhut <dhersz@gmail.com>
2023-11-09 10:49:24 -0300

    package setup and assert_osmium_is_installed()

---
+++ R/assert_osmium_is_installed.R
@@ -0,0 +1,26 @@
+#' Assert Osmium is installed
+#'
+#' Asserts Osmium is installed, throwing an error otherwise.
+#'
+#' @return Throws an error if Osmium is not installed, invisibly returns `TRUE`
+#'   otherwise.
+#'
+#' @examplesIf identical(tolower(Sys.getenv("NOT_CRAN")), "true")
+#' assert_osmium_is_installed()
+#'
+#' @export
+assert_osmium_is_installed <- function() {
+  is_installed <- tryCatch(
+    processx::run("osmium", "--version"),
+    error = function(cnd) cnd
+  )
+
+  if (inherits(is_installed, "error")) {
+    stop(
+      "Could not find osmium in system. ",
+      "Please make sure it has been installed and added to PATH."
+    )
+  }
+
+  return(invisible(TRUE))
+}
