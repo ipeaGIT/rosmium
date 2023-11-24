d3742b45ee6d5b4db86c11c8ab0cbfef1a569009
Daniel Herszenhut <dhersz@gmail.com>
2023-11-24 12:35:55 -0300

    package-level documentation

---
+++ R/rosmium.R
@@ -0,0 +1,20 @@
+#' rosmium: Bindings for Osmium Tool
+#'
+#' Allows one to use Osmium Tool from R. Osmium is a multipurpose command line
+#' tool that enables one to manipulate and analyze OpenStreetMap (OSM) files
+#' through several different commands. Currently, this package does not aim to
+#' offer functions that cover the entirety of Osmium’s API, instead making
+#' available functions that wrap only a very limited set of Osmium’s features.
+#'
+#' @section Usage:
+#' Please check the vignette for more on the package usage:
+#' - Introductino to rosmium: R bindings for Osmium Tool. Run
+#' `vignette("rosmium")` or check it on the
+#' [website](https://ipeagit.github.io/rosmium/articles/rosmium.html).
+#'
+#' @docType package
+#' @name rosmium
+#' @aliases rosmium-package
+#'
+#' @keywords internal
+"_PACKAGE"
